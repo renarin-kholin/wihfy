4cdb97a9610012194cef13678280153f701a7c7f
Shishu <shishudesu@gmail.com>
2024-08-03 22:37:29 +0530

    Fixed minor bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const wihy_list: string[]= [
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col  gap-4 p-24 text-left w-[700px] mx-auto bg-white">
-      <h1 className="text-2xl font-bold text-left text-black">What I haven't finished yet: </h1>
+      <h1 className="text-2xl font-bold text-left text-black">{'What I haven\'t finished yet: '}</h1>
       <ul className="gap-3 flex flex-col">
         {wihy_list.map((i, index) => <ListItem key={index}>{i}</ListItem>)}
       </ul>
