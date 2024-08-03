1aaf83ce8fbaba368c313a7bbc6e5f2cbf78ed9e
Shishu <shishudesu@gmail.com>
2024-08-03 22:34:21 +0530

    Initial Commit

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ListItem({children}: {children: React.ReactNode}) {
+    return (
+        <li className="flex flex-row gap-2 items-center">
+            <div className=" h-4 w-4 bg-red-500"></div>
+            <p className="text-base antialiased selection:bg-black font-semibold">&#160;{children}&#160;</p>
+        </li>
+    )
+}
