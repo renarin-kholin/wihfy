c734b0b7a1f0317a0ddb3812040e4ff887dbe68f
Shishu <shishudesu@gmail.com>
2024-08-07 17:26:46 +0530

    Now also using -webkit-tap-highlight-color extension for ios safari

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -4,7 +4,7 @@ export default function ListItem({children}: {children: React.ReactNode}) {
     return (
         <li className="flex flex-row gap-2 items-center">
             <div className=" h-4 w-4 bg-red-500"></div>
-            <p className="text-base antialiased selection:bg-black font-semibold">&#160;{children}&#160;</p>
+            <p className="text-base antialiased selection:bg-black font-semibold selection:[-webkit-tap-highlight-color:rgba(0,0,0,0)]">&#160;{children}&#160;</p>
         </li>
     )
 }
